64c91ef7d4eb350a895be98240d794eb19975168
a-jsoffner <a-jsoffner@SAO6-L02006.ucnet.g-ushin.com>
2022-01-06 22:15:08 -0300

    Teste2

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,9 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 public static void main(String[] args) {
-	System.out.println("Meu Primeiro Programa em Java");
+	System.out.println("Meu Primeiro Programa em Java2, teste 3");
+	System.out.println("Meu Primeiro Programa em Java2, teste 4");
+	System.out.println("Meu Primeiro Programa em Java2, teste 4");
 }
 
 }
